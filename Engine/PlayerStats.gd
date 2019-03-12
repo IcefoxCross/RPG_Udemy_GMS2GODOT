b0e2708bc70d8203f76b5103a989db1ec426362f
IcefoxCross <icefox.990@gmail.com>
2019-03-12 13:39:29 -0300

    section 7/class 22

--- Engine/PlayerStats.gd
+++ Engine/PlayerStats.gd
@@ -18,6 +18,19 @@ func _ready():
 	
 	draw_health = stats["health"]
 
+func _process(delta):
+	update_draw_health()
+	# Update stats for death and level up
+	if draw_health <= 0:
+		get_tree().change_scene("res://MainScenes/GameOver.tscn")
+	# Level up
+	if stats["experience"] >= stats["maxexperience"]:
+		level += 1
+		stats["experience"] = stats["experience"] - stats["maxexperience"]
+		stats["maxexperience"] = level * 10
+		stats["health"] = calculate_health(level, _class["health"])
+		stats["maxhealth"] = stats["health"]
+
 func calculate_health(level, health):
 	return round((health*3)+(level*3))
 
@@ -28,3 +41,10 @@ func get_stats_from_class(c):
 	if c == "elizabeth":
 		stats["maxexperience"] = level * 10
 	return stats
+
+func update_draw_health():
+	var dis = draw_health - stats["health"]
+	if dis > 1:
+		draw_health = lerp(draw_health, stats["health"], .1)
+	else:
+		draw_health = stats["health"]
